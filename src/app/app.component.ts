8d9cec80aa9095eab464365b762ba11744552b97
Anicod <animeshraj735@gmail.com>
2022-11-04 03:45:54 +0530

    added: name using input with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Hello World";
   url = "../assets/bridge.png"
+  userName = ""
+  bridgeLabzUrl = "https://www.bridgelabz.com/"
+  nameErr = ""
   ngOnInit(): void{
-    this.title = "Hell0 world from bridgelabz"
+    this.title = "from bridgelabz"
+  }
+  getBridgeLabzPage(){
+    window.open(this.bridgeLabzUrl);
+  }
+  onInput($event:any){
+    console.log("hello", $event.data)
+    const nameRegx = RegExp('[A-Z]{1}[a-zA-Z\\s]{2,}$')
+    if(nameRegx.test(this.userName)){
+      this.nameErr = ""
+      return;
+    }
+    this.nameErr = "name is not valid"
   }
 }
